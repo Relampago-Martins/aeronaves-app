d34e7690a470f408d80e069e17dd4ec3e5ac41d8
bgmartins <bgmartin@ucs.br>
2026-06-30 13:01:06 -0300

    chore(deps): 🔧 Downgrade para expo funcionar no iphone
    * Atualiza as versões das dependências principais, incluindo `expo`, `react`, e `react-native`.
    * Substitui `expo-router` por `@react-navigation/native` no arquivo `_layout.tsx`.
    * Ajusta as versões das dependências de desenvolvimento, como `typescript` e `@types/react`.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,6 @@
-import { DefaultTheme, ThemeProvider } from "expo-router";
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { useColorScheme } from "react-native";
 
-import { AnimatedSplashOverlay } from "@/components/animated-icon";
 import HomeScreen from ".";
 
 export default function TabLayout() {
